0bbe0c03924c92feef2239c263da5b4f98f55c24
SamuelVersiane <samuellversiane@gmail.com>
2021-11-07 20:24:10 -0300

    Barchart correction

--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -1,4 +1,4 @@
-import axios, { Axios } from 'axios'
+import axios from 'axios'
 import { useEffect, useState } from 'react'
 import Chart from 'react-apexcharts'
 import { SaleSuccess } from 'type/sale'
