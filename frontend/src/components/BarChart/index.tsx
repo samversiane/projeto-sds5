179a6c6ec29a4c90fe730dd2406e42a9f3bf6e86
SamuelVersiane <samuellversiane@gmail.com>
2021-11-07 17:44:06 -0300

    DataTable integration

--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -60,19 +60,6 @@ const BarChart = () => {
         },
     };
 
-    //Site estático
-    // const mockData = {
-    //     labels: {
-    //         categories: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    //     },
-    //     series: [
-    //         {
-    //             name: "% Sucesso",
-    //             data: [43.6, 67.1, 67.7, 45.6, 71.1]
-    //         }
-    //     ]
-    // };
-
     return (
         <Chart
             options={{ ...options, xaxis: chartData.labels }}
--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -24,25 +24,6 @@ const DonutChart = () => {
             });
     }, [])
 
-    //Forma errada
-    // let chartData: ChartData = { labels: [], series: [] }
-
-    // axios.get(`${BASE_URL}/sales/amount-by-seller`)
-    //     .then(response => {
-    //         const data = response.data as SaleSum[]
-    //         const mySeries = data.map(x => x.sum)
-    //         const myLabels = data.map(x => x.seller)
-
-    //         chartData = {labels: myLabels, series: mySeries}
-    //         console.log(chartData)
-    //     });
-
-    //Página estática
-    // const mockData = {
-    //     series: [477138, 499928, 444867, 220426, 473088],
-    //     labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    // }
-
     const options = {
         legend: {
             show: true
